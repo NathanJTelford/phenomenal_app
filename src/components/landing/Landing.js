b2d8d71919c83228c78a87daeccf9d7da8354b00
NathanJTelford <nathan.j.i.telford@gmail.com>
2019-03-20 15:29:50 -0600

    initial commit

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+       <h1>Welcome to a website...</h1>
+       <p>we're still not quite sure what it does, but take a look around and you'll see.</p>
+      </div>
+    )
+  }
+}
